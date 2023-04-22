72914b496e7c90c31ba242f029c89e0823981154
Orion <97639175+RajaGiddi@users.noreply.github.com>
2023-04-22 18:35:43 +0000

    Re-did the homepage a bit, still needs work but for now it's got a very basic landing page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonToolbar } from '@ionic/react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>ToDo</IonTitle>
-        </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent fullscreen className={isDarkMode ? 'dark' : ''}>
         <div className="container">
-          <Link to="/login">Login</Link>
+          <div className="title-container">
+            <h1 className="header-title">ToDo.AI</h1>
+            <p>Where productivity meets AI</p>
+          </div>
+          <div className="button-container">
+            <Link to="/login">
+              <IonButton color="primary" expand="block" style={{ width: 'auto' }}>Get Started</IonButton>
+            </Link>
+          </div>
         </div>
-        </IonContent>
+      </IonContent>
     </IonPage>
   );
 };
